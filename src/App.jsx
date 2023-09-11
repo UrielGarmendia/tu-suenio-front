df4232c7299888359f733aa8788a85c6a6af4a5c
UrielGarmendia <garmendiauriel@gmail.com>
2023-09-11 20:18:57 -0300

    hola

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,7 @@ function App() {
   return (
     <>
       <h1>Tu sueño</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia totam
-        vero esse doloremque corporis, assumenda consequuntur maiores ea impedit
-        praesentium inventore qui blanditiis illo, corrupti perspiciatis facilis
-        molestias aperiam deserunt?
-      </p>
+      <h3>hola</h3>
     </>
   );
 }
