46b0ad45776435e0c719c7c1092500129b686aba
kevincalle3101 <kevinmendoza3101@hotmail.com>
2023-09-12 17:08:56 -0500

    renderizado de cards en alcancías

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Landing from "./Components/Landing/Landing";
+import Alcancias from "./Components/Alcancias/Alcancias";
 function App() {
   const location = useLocation();
   return (
     <div >
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/alcancias" element={<Alcancias />} />
       </Routes>
       {location.pathname !== "/"}
     </div>
---
+++ src/Components/Alcancias/Alcancias.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Card from '../Card/Card'
+
+const Alcancias = () => {
+    const [alcancias, setAlcancias] = useState([])
+    useEffect(() => {
+        try {
+            const getAlcancias = async () => {
+                const { data } = await axios.get("https://fakestoreapi.com/products");
+                setAlcancias(data)
+            }
+            getAlcancias();
+        } catch (error) {
+            console.error("error al obtener los productos", error)
+        }
+    }, [])
+    return (
+        <div>
+            {alcancias? alcancias.map(({id, title, price, image}) => {
+                return(
+                <Card id={id} title={title} price={price} image={image} stock={id}/>)})
+            : <h1>Cargando...</h1>}
+        </div>
+    )
+}
+
+export default Alcancias
