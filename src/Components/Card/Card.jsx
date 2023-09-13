ac03de6a6e171411d8821d78a62d2d2b63d69af6
AlejoA <speedmax159@gamil.com>
2023-09-12 19:41:03 -0500

    Css de las cards

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Card.css"
 
 const Card = ({ id, title, price, stock, image}) => {
   return (
     <div key={id} className="card-container">
       <img src={image} alt={title} className="card-image"/>
-      <h2>{title}</h2>
+      <h3 className="card-title">{title}</h3>
       <h3 className="card-price">Precio: {price}</h3>
       <h3>Stock disponible: {stock}</h3>
       <h3>Temática:</h3>
