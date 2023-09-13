480beb0a1ca52888c1f61fe8b9ca7f3ad0d26bbf
Djoanjaramillo <joanjaramillo_9@hotmail.com>
2023-09-13 16:38:23 -0500

    home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import fiestas from "./fiestas.jpg"
 import styles from "../Home/Home.module.css"
+import fiestas from "./fiesta.png"
 import plan from "./plan.png"
+import dequesirve from "./dequesirve.png"
+import gafas from "./gafas.png"
+import telefono from "./telefono.png"
+import credito from "./credito.png"
+import mujer from "./mujer.png"
+import plandeahorro from "./plan de ahorro.jpg"
 
 
  const home = () => {
@@ -12,10 +18,15 @@ import plan from "./plan.png"
             <img src={plan} alt="plan" className={styles.planLogo} />
           </div>
           <div className={styles.textContainer}>
-            <h2>por que es importante el plan de ahorro ?</h2>
+            <h2>por que es importante ahorrar ?</h2>
             <p>
-              Tener un plan de ahorro debería ser importante para poder llevar un
-              orden de ahorro, así va a ser más fácil llegar al éxito del ahorro.
+            Ahorrar te permite tener un colchón de dinero disponible 
+            para emergencias inesperadas, como gastos médicos, reparaciones
+            de automóviles o pérdida de empleo. Esto evita que
+            te endeudes o te veas en situaciones financieras difíciles; tambien
+            Ahorrar te ayuda a trabajar hacia metas financieras a corto y largo plazo, 
+            como comprar una casa, pagar la educación de tus hijos, tomar unas vacaciones
+             soñadas o jubilarte cómodamente.
             </p>
             <div className={styles.informacion}>
                 <p className={styles.masinfop}>Mas informacion</p>
@@ -37,6 +48,100 @@ import plan from "./plan.png"
           </div>
           <div className={styles.imageContainer}>
             <img src={fiestas} alt="fiestas" className={styles.fiestaLogo} />
+          </div>
+        </div>
+        <div className={styles.contentContainer}>
+          <div className={styles.imageContainer}>
+            <img src={dequesirve} alt="dequesirve" className={styles.planLogo} />
+          </div>
+          <div className={styles.textContainer}>
+            <h2>por que es importante tener un   plan de ahorro ?</h2>
+            <p>
+            Un plan de ahorro te impulsa a ser disciplinado con tus finanzas personales.
+             Al asignar una parte de tus ingresos para el ahorro de manera regular, desarrollas
+              hábitos financieros saludables y evitas el gasto excesivo; Ahorrar según un plan te
+               brinda seguridad financiera. Sabes que tienes recursos disponibles para lidiar 
+              con imprevistos y para alcanzar tus metas financieras a largo plazo. Esto reduce
+               el estrés relacionado con las preocupaciones financieras.
+            </p>
+            <div>
+            <img src={plandeahorro} alt="plandeahorro" className={styles.imgAhorro} />
+            </div>
+            <div className={styles.informacion2}>
+                <p className={styles.masinfop}>Mas informacion</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.contentContainer}>
+          <div className={styles.textContainer}>
+            <h2>no se que mas preguntar?</h2>
+            <p>
+              Nuestras alcancías sirven para cualquier tipo de evento, ya que
+              puedes personalizarlas a tu gusto y usarlas como por ejemplo:
+              recordatorios en bodas, fiestas infantiles, prácticamente para lo
+              que quieras.
+            </p>
+            <div className={styles.informacion1}>
+                <p className={styles.masinfop}>Mas informacion</p>
+            </div>
+          </div>
+          <div className={styles.imageContainer}>
+            <img src={gafas} alt="gafas" className={styles.fiestaLogo} />
+          </div>
+        </div>
+        <div className={styles.contentContainer}>
+          <div className={styles.imageContainer}>
+            <img src={telefono} alt="telefono" className={styles.planLogo} />
+          </div>
+          <div className={styles.textContainer}>
+            <h2>no se que mas preguntar ?</h2>
+            <p>
+            Un plan de ahorro te impulsa a ser disciplinado con tus finanzas personales.
+             Al asignar una parte de tus ingresos para el ahorro de manera regular, desarrollas
+              hábitos financieros saludables y evitas el gasto excesivo; Ahorrar según un plan te
+               brinda seguridad financiera. Sabes que tienes recursos disponibles para lidiar 
+              con imprevistos y para alcanzar tus metas financieras a largo plazo. Esto reduce
+               el estrés relacionado con las preocupaciones financieras.
+            </p>
+            <div className={styles.informacion}>
+                <p className={styles.masinfop}>Mas informacion</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.contentContainer}>
+          <div className={styles.textContainer}>
+            <h2>no se que mas preguntar?</h2>
+            <p>
+              Nuestras alcancías sirven para cualquier tipo de evento, ya que
+              puedes personalizarlas a tu gusto y usarlas como por ejemplo:
+              recordatorios en bodas, fiestas infantiles, prácticamente para lo
+              que quieras.
+            </p>
+            <div className={styles.informacion1}>
+                <p className={styles.masinfop}>Mas informacion</p>
+            </div>
+          </div>
+          <div className={styles.imageContainer}>
+            <img src={credito} alt="credito" className={styles.fiestaLogo} />
+          </div>
+        </div>
+        <div className={styles.contentContainer}>
+          <div className={styles.imageContainer}>
+            <img src={mujer} alt="mujer" className={styles.planLogo} />
+          </div>
+          <div className={styles.textContainer}>
+            <h2>no se que mas preguntar ?</h2>
+            <p>
+            Un plan de ahorro te impulsa a ser disciplinado con tus finanzas personales.
+             Al asignar una parte de tus ingresos para el ahorro de manera regular, desarrollas
+              hábitos financieros saludables y evitas el gasto excesivo; Ahorrar según un plan te
+               brinda seguridad financiera. Sabes que tienes recursos disponibles para lidiar 
+              con imprevistos y para alcanzar tus metas financieras a largo plazo. Esto reduce
+               el estrés relacionado con las preocupaciones financieras.
+            </p>
+            <div className={styles.informacion}>
+                <p className={styles.masinfop}>Mas informacion</p>
+            </div>
           </div>
         </div>
       </div>
