765ac0fd049856b3b28c618697bcab613687cc90
Djoanjaramillo <joanjaramillo_9@hotmail.com>
2023-09-13 12:07:44 -0500

    idea de home

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Landing from "./Components/Landing/Landing";
 import Alcancias from "./Components/Alcancias/Alcancias";
+import Home from "./Components/Home/Home";
 function App() {
   const location = useLocation();
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/alcancias" element={<Alcancias />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
       {location.pathname !== "/"}
     </div>
