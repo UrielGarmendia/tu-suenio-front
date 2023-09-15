db17e1a0145b0e7f48298bc4eafdc9ee4dd6c21e
leorega <leonardoregazzoni@gmail.com>
2023-09-15 12:40:36 -0300

    Agrego navigate a Card para que lleve a Detail

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Card.css"
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { useNavigate } from "react-router-dom";
 
 const Card = ({ id, name, title, price, stock, image }) => {
+
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(`/detail/${id}`);
+  };
+
   return (
-    <div key={id} className="card-container">
+    <div key={id} onClick={handleNavigate} className="card-container">
       <AddShoppingCartIcon className="iconAddShoppingCart"/>
       <img src={image} alt={title} className="card-image"/>
       <h3 className="card-title">{title}</h3>
