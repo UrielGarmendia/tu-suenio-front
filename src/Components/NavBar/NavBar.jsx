3f8b0ab3a92a170983960b7d3a71979ad914388e
kevincalle3101 <kevinmendoza3101@hotmail.com>
2023-09-17 22:51:53 -0500

    primeros cambios

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,34 +4,36 @@ import logoUser from '../../Assents/logoUser.png'
 import carrito from '../../Assents/carrito.png'
 import { Link } from 'react-router-dom'
 import SearchBar from '../SearchBar/SearchBar'
-import { useNavigate } from "react-router-dom";
+import { useAuth0 } from '@auth0/auth0-react'
+import SlideMenu from '../SlideMenu/SlideMenu'
 
 const NavBar = () => {
-  const navigate = useNavigate();
-  const handleNavigate = () => {
-    navigate(`/login`);
-  };
 
-    return(
-      <div className={style.navBar}>
-        <div><img src={logo} alt="logo" height='100px'/></div>
-        <div className={style.botones}>
-          <div>
-            <Link to='/'><button>INICIO</button></Link>
-            <Link to='/create'><button>CREAR ALCANCIA</button></Link>
-            <Link to='/alcancias'><button>ALCANCIAS</button></Link>        
-            <button>EMPRESA</button>
-            <button>CONTACTANOS</button>
-          </div>
+  const { loginWithRedirect, isAuthenticated, user } = useAuth0();
+
+  return (
+    <div className={style.navBar}>
+      <div><img src={logo} alt="logo" height='100px' /></div>
+      <div className={style.botones}>
+        <div>
+          <Link to='/'><button>INICIO</button></Link>
+          <Link to='/create'><button>CREAR ALCANCIA</button></Link>
+          <Link to='/alcancias'><button>ALCANCIAS</button></Link>
+          <button>EMPRESA</button>
+          <button>CONTACTANOS</button>
         </div>
-        <SearchBar/>
-        <div className={style.signin}>
-          <img src={carrito} alt="carrito" />
-        <img src={logoUser} alt="icono user"></img>
-          <button onClick={handleNavigate}>Iniciar sesion</button>
-        </div>
+      </div>
+      <SearchBar />
+      <div className={style.signin}>
+        <img src={carrito} alt="carrito" />
+        {isAuthenticated ?
+          <SlideMenu />
+          :
+          <button onClick={() => loginWithRedirect()}>Iniciar sesion</button>
+        }
+      </div>
     </div>
-   )
-   }
+  )
+}
 
-   export default NavBar
+export default NavBar
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Auth0Provider} from '@auth0/auth0-react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './Redux/Store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-higol6lp8oflnbji.us.auth0.com"
+    clientId="a8xVvNi2ttwTl0n4NVQtHVFrX7kRnPAi"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
+    </Auth0Provider>
   </React.StrictMode>,
 )
