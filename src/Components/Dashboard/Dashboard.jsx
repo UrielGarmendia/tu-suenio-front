49d47022f1bc087e524febc000e9ff1b94fe6dd2
PAULGN1992 <47513033@continental.edu.pe>
2023-09-21 01:33:53 -0500

    Agregue la funcion de cada boton del menu lateral y arregle los estilos del dashboard

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,37 +1,12 @@
-import Graficos from '../Graficos/Graficos'
 import style from '../Dashboard/Dashboard.module.css'
-import { useState } from 'react';
 import { Add, ManageAccounts, PointOfSale, Settings, TrendingFlat, TrendingUpOutlined } from '@mui/icons-material';
-
+import Estadisticas from "../Estadisticas/Estadisticas";
+import Create from "../Create/Create"
+import { useState } from 'react';
 
 const Dashboard = () => {
 
-  const [activeGraph, setActiveGraph] = useState('ventasDia');
-
-  const data1 = [
-    ['Venta de producto', 'Cantidad'],
-    ['Alcancias pequeñas',  40],
-    ['Alcancias medianas',  10],
-    ['Alcancias grandes',  20]
-  ];
-  const options1 = {
-    title: "Ventas del día",
-    is3D: true,
-    legend: { position: 'left' }
-  };
-
-  const data2 = [
-    ['Mes', 'Julio','Agosto', 'Septiembre'],
-    ['Alcancias pequeñas',  400, 1000,      400],
-    ['Alcancias medianas',  200, 1170,      460],
-    ['Alcancias grandes',  400, 660,       1120],
-  ];
-  const options2 = {
-    title: "Ventas del mes",
-    curveType: 'function',
-    legend: { position: 'rigth' }
-  };
-
+  const [activeButton, setActiveButton] = useState('Estadisticas')
 
     return (
     <div className={style.dashboard}>
@@ -39,34 +14,31 @@ const Dashboard = () => {
         <ul>
           <li><p><Add></Add>PRODUCTOS</p>
             <ul>
-              <li><p>ACTUALIZAR PRODUCTO</p></li>
-              <li><p>CREAR PRODUCTO</p></li>
-              <li><p>ELIMINAR PRODUCTO</p></li>
+              <li onClick={()=>setActiveButton('ActProducto')}><p>ACTUALIZAR PRODUCTO</p></li>
+              <li onClick={()=>setActiveButton('Productos')}><p>CREAR PRODUCTO</p></li>
+              <li onClick={()=>setActiveButton('ElProducto')}><p>ELIMINAR PRODUCTO</p></li>
             </ul>
           </li>
-          <li><p><PointOfSale></PointOfSale>REPORTE DE VENTAS</p></li>
-          <li><p><TrendingUpOutlined></TrendingUpOutlined>GRAFICOS</p></li>
-          <li><p><ManageAccounts></ManageAccounts>USUARIOS REGISTRADOS</p></li>
-          <li><p><Settings></Settings>CONFIGURACION DE PERFIL</p></li>
+          <li onClick={()=>setActiveButton('RepVentas')}><p><PointOfSale></PointOfSale>REPORTE DE VENTAS</p></li>
+          <li onClick={()=>setActiveButton('Estadisticas')}><p><TrendingUpOutlined></TrendingUpOutlined>ESTADÍSTICAS</p></li>
+          <li onClick={()=>setActiveButton('UserReg')}><p><ManageAccounts></ManageAccounts>USUARIOS REGISTRADOS</p></li>
+          <li onClick={()=>setActiveButton('Config')}><p><Settings></Settings>CONFIGURACIÓN DE PERFIL</p></li>
         </ul>
       </section>
-      <div className={style.graficos}>
-        <div className={style.ventas}>
-          <span onClick={() => setActiveGraph('ventasTotales')}><div>Ventas Totales<p>$114000</p></div><div>%45</div></span>
-          <span onClick={() => setActiveGraph('ventasDia')}><div>Ventas del día<p>$114000</p></div><div>%45</div></span>
-          <span><div>Ordenes Totales<p>$114000</p></div><div>%45</div></span>
-          <span><div>Clientes Totales<p>$114000</p></div><div>%45</div></span>
-        </div>
+      <div className={style.container}>
+      {activeButton === 'Estadisticas' && ( <Estadisticas/>)}
+      {activeButton === 'Productos' && (<Create/>)}
+      {activeButton === 'ActProducto' && (<h1>Aqui ira la seccion de Actualizar productos</h1>)}
+      {activeButton === 'ElProducto' && (<h1>Aqui ira la seccion de PRODUCTOS A ELIMINAR</h1>)}
+      {activeButton === 'RepVentas' && (<h1>Aqui ira la seccion REPORTE DE VENTAS</h1>)}
+      {activeButton === 'UserReg' && (<h1>Aqui ira la seccion USUARIOS REGISTRADOS</h1>)}
+      {activeButton === 'Config' && (<h1>Aqui ira la seccion de CONFIGURACION DEL ADMIN</h1>)}
+      <article>
+       <h1>ALGUN OTRO COMPONENTE PODRIA IR ACA</h1>
+      </article>
+     
+      </div>
 
-      <div>
-        {activeGraph === 'ventasDia' && (
-            <Graficos type="PieChart" options={options1} data={data1} />
-          )}
-          {activeGraph === 'ventasTotales' && (
-            <Graficos type="LineChart" options={options2} data={data2} />
-        )}
-      </div>
-      </div>
     </div>
   );
 };
