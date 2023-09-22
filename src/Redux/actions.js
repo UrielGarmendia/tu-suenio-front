debfe8f5192a928e89e2476f77aef9b2a36b1af7
AlejoA <speedmax159@gamil.com>
2023-09-22 17:03:03 -0500

    Persistencias de datos en el carrito y persistencia de logueo en cualquier navegador

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -15,9 +15,6 @@ import {
 } from "./actions-types";
 import axios from "axios";
 
-console.log();
-
-// Obtener lo que serían todas las alcancías
 export const allAlcancias = () => {
   try {
     return async function (dispatch) {
@@ -82,7 +79,7 @@ export const byName = (name) => {
   try {
     return async function (dispatch) {
       const { data } = await axios(
-        `https://tu-suenio-back.onrender.com/?name=${name}`
+        `https://tu-suenio-back.onrender.com/products?name=${name}`
       );
       return dispatch({
         type: BY_NAME,
