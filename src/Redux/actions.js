8b8d46415e08c57512763a22f4550445ca9df348
PAULGN1992 <47513033@continental.edu.pe>
2023-09-28 18:47:28 -0500

    Agregue ventana modal y exportacion en excel

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -22,7 +22,7 @@ export const allAlcancias = () => {
   try {
     return async function (dispatch) {
       const { data } = await axios(
-        "https://tu-suenio-back.onrender.com/products"
+        "https://localhost:3001/products"
       );
       return dispatch({
         type: ALCANCIAS,
@@ -38,7 +38,7 @@ export const detail = (id) => {
   try {
     return async function (dispatch) {
       const { data } = await axios(
-        `https://tu-suenio-back.onrender.com/products/${id}`
+        `http://localhost:3001/products/${id}`
       );
       return dispatch({
         type: DETAIL,
@@ -59,7 +59,7 @@ export function cleanDetail() {
 export const createAlcancias = (newProduct) => {
   return async function (dispatch) {
     const response = await axios.post(
-      `https://tu-suenio-back.onrender.com/products/create`,
+      `http://localhost:3001/products/create`,
       newProduct
     );
     return dispatch({
@@ -73,7 +73,7 @@ export const categories = () => {
   try {
     return async function (dispatch) {
       const { data } = await axios(
-        "https://tu-suenio-back.onrender.com/categorie"
+        "http://localhost:3001/categorie"
       );
       return dispatch({
         type: CATEGORIES,
@@ -88,7 +88,7 @@ export const categories = () => {
 export const byName = (name) => {
   return async function (dispatch) {
     try {
-      const response = await axios.get(`https://tu-suenio-back.onrender.com/products?name=${name}`);
+      const response = await axios.get(`http://localhost:3001/products?name=${name}`);
       dispatch({
         type: BY_NAME,
         payload: response.data,
@@ -107,7 +107,7 @@ export const filtered = (id) => {
   try {
     return async function (dispatch) {
       const { data } = await axios(
-        `https://tu-suenio-back.onrender.com/filter/categorie/${id}`
+        `http://localhost:3001/filter/categorie/${id}`
       );
       return dispatch({
         type: FILTERED_BY,
@@ -122,7 +122,7 @@ export const filterBySize = (size) => {
   return async function (dispatch) {
     try {
       const { data } = await axios(
-        `https://tu-suenio-back.onrender.com/filter/size/${size}`
+        `http://localhost:3001/filter/size/${size}`
       );
       return dispatch({
         type: FILTERED_BY_SIZE,
@@ -137,7 +137,7 @@ export const ProductsByCategoryAndSize = (id, size) => {
   return async function (dispatch) {
     try {
       const { data } = await axios(
-        `https://tu-suenio-back.onrender.com/filter/combined/${id}/${size}`
+        `http://localhost:3001/filter/combined/${id}/${size}`
       );
       return dispatch({
         type: PRODUCTS_BY_CATEGORIEANDSIZE,
@@ -158,7 +158,7 @@ export const CartShopping = (id) => {
   try {
     return async function (dispatch) {
       const { data } = await axios(
-        `https://tu-suenio-back.onrender.com/products/${id}`
+        `http://localhost:3001/products/${id}`
       );
       return dispatch({
         type: CART_SHOPING,
@@ -187,11 +187,11 @@ export const actualizarProduct = (id, updatedProductData) => {
   try {
     return async function (dispatch) {
       await axios.put(
-        `https://tu-suenio-back.onrender.com/products/${id}`,
+        `http://localhost:3001/products/${id}`,
         updatedProductData
       );
       const { data } = await axios(
-        `https://tu-suenio-back.onrender.com/products`
+        `http://localhost:3001/products`
       );
       return dispatch({
         type: ACTUALIZAR_PRODUCTO,
@@ -207,10 +207,10 @@ export const deleteProduct = (id) => {
   try {
     return async function (dispatch) {
       await axios.delete(
-        `https://tu-suenio-back.onrender.com/products/${id}`
+        `http://localhost:3001/products/${id}`
       );
       const { data } = await axios(
-        `https://tu-suenio-back.onrender.com/products`
+        `http://localhost:3001/products`
       );
       return dispatch({
         type: DELETE_PRODUCT,
